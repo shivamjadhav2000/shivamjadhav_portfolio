5a4ed605dba0a766e0db64fec46f57c188f776ca
shivamjadhav <shivamjadhav348@gmail.com>
2024-07-14 17:25:44 -0500

    added favicon

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const main = () => {
+document.addEventListener('DOMContentLoaded', () => {
   var currslide = 0
   var slides = document.querySelectorAll('.slide')
-  console.log('slides', slides)
   var maxslides = slides.length
   const slideanimation = () => {
-    console.log('slideanimation', currslide, maxslides)
     currslide = (currslide + 1) % maxslides
     slides.forEach((s, i) => {
       s.style.display = `${i === currslide ? 'block' : 'none'}`
     })
   }
   setInterval(slideanimation, 6000)
-}
-document.addEventListener('DOMContentLoaded', main)
+})
 
